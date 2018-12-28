0e61eabdfb073c871e6d2c3394f082924334c833
Greewi <greewi.feerie@gmail.com>
2018-12-28 18:10:24 +0100

    Meilleur gestion des tableaux et des encarts + quelques ajustements mineurs.

--- odtConverter.js
+++ odtConverter.js
@@ -77,19 +77,34 @@ exports.ODTConverter = class {
                         return `<h3>${this._convertInternalNode(node, rootLevel)}</h3>\n`;
                     return `<h2>${this._convertInternalNode(node, rootLevel)}</h2>\n`;
                 case "p" :
-                    if(node.hasAttribute("class") && node.getAttribute("class")=="corps-de-liste-western")
-                        return `    <li>${this._convertInternalNode(node, rootLevel)}</li>\n`;
-                    if(node.hasAttribute("class") && node.getAttribute("class")=="corps-de-liste")
-                        return `    <li>${this._convertInternalNode(node, rootLevel)}</li>\n`;
-                    if(node.hasAttribute("class") && node.getAttribute("class")=="corps-de-citation-western")
-                        return `<blockquote>${this._convertInternalNode(node, rootLevel)}</blockquote>\n`;   
-                    if(node.hasAttribute("class") && node.getAttribute("class")=="corps-de-citation")
-                        return `<blockquote>${this._convertInternalNode(node, rootLevel)}</blockquote>\n`;                 
+                    if(node.hasAttribute("class"))
+                    {
+                        switch(node.getAttribute("class"))
+                        {
+                            case "corps-de-liste":
+                            case "corps-de-liste-western":
+                            return `<ul>\n\t<li>${this._convertInternalNode(node, rootLevel)}</li>\n</ul>\n`;
+
+                            case "corps-de-citation":
+                            case "corps-de-citation-western":
+                            return `<blockquote>${this._convertInternalNode(node, rootLevel)}</blockquote>\n`;
+
+                            case "encart---titre":
+                            case "encart---titre-western":
+                            return `<div class="encart">\n\t<h2 class="encart__titre">${this._convertInternalNode(node, rootLevel)}</h2>\n</div>\n`; 
+                            
+                            case "encart":
+                            case "encart-western":
+                            return `<div class="encart">\n\t<p class="encart__texte">${this._convertInternalNode(node, rootLevel)}</p>\n</div>\n`;
+                            
+                            case "encart---liste":
+                            case "encart---liste-western":
+                            return `<div class="encart">\n\t<ul>\n\t\t<li class="encart__liste">${this._convertInternalNode(node, rootLevel)}</li>\n\t</ul>\n</div>\n`;
+                        }
+                    }
                     return `<p>${this._convertInternalNode(node, rootLevel)}</p>\n`
                 case "br" :
                     return "<br/>\n";
-                case "ul" :
-                    return `<ul>\n${this._convertInternalNode(node, rootLevel)}</ul>\n`;
                 case "samp" : 
                     return `<cite>${this._convertInternalNode(node, rootLevel)}</cite>`
                 case "em" : 
@@ -103,10 +118,17 @@ exports.ODTConverter = class {
                 case "tr" :
                     return `\t<tr>\n${this._convertInternalNode(node, rootLevel)}\t</tr>\n`
                 case "th" :
-                    return `\t\t<th>${this._convertInternalNode(node, rootLevel)}</th>\n`
+                    if(node.hasAttribute("colspan"))
+                        return `\t\t<th colspan="${node.getAttribute("colspan")}">${this._convertInternalNode(node, rootLevel)}</th>\n`
+                    else
+                        return `\t\t<th>${this._convertInternalNode(node, rootLevel)}</th>\n`
                 case "td" :
-                    return `\t\t<td>${this._convertInternalNode(node, rootLevel)}</td>\n`
+                    if(node.hasAttribute("colspan"))
+                        return `\t\t<td colspan="${node.getAttribute("colspan")}">${this._convertInternalNode(node, rootLevel)}</td>\n`
+                    else
+                        return `\t\t<td>${this._convertInternalNode(node, rootLevel)}</td>\n`
                 case "body" :
+                case "ul" :
                 case "li" :
                 case "a" :
                 case "i" :
@@ -129,14 +151,17 @@ exports.ODTConverter = class {
 
     static _mergeSimilarTag(html){
         return html
-            .replace(/<\/em><em>/gi, "")
-            .replace(/<\/cite><cite>/gi, "");
+            .replace(/\s*<\/em>\s*<em>/gi, "")
+            .replace(/\s*<\/div>\s*<div[^>]*>/gi, "")
+            .replace(/\s*<\/ul>\s*<ul[^>]*>/gi, "")
+            .replace(/\s*<\/cite>\s*<cite>/gi, "");
     }
 
     static _deleteEmptyTag(html){
         return html
-        .replace(/<p><\/p>\n?/gi, "")
-        .replace(/(<td>|<th>)<p>/gi, "$1")
-        .replace(/<\/p>[\s]*(<\/td>|<\/th>)/gi, "$1");
+        .replace(/\s*<p>\s*<\/p>/gi, "")
+        .replace(/\s*<p>\s*<br\/>\s*<\/p>/gi, "")
+        .replace(/(<(td|th)(\s*colspan="[^"]*")?>)\s*<p>/gi, "$1")
+        .replace(/<\/p>\s*(<\/(td|th)>)/gi, "$1");
     }
 }
